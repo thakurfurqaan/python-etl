e183dad328dc425ec683f6237428a38246fd3a58
Furqaan Thakur <thakurfurqaan@gmail.com>
2023-01-10 23:07:48 +0400

    Added postgres engine creation

--- database.py
+++ database.py
@@ -1,4 +1,21 @@
 import sqlalchemy
+from dataclasses import dataclass
 
+@dataclass
 class Database:
-    pass
+    DB_HOST: str = None
+    DB_PORT: str = None
+    DB_USERNAME: str = None
+    DB_PASSWORD: str = None
+    DB_TYPE: str = None
+
+    def get_engine(self):
+        from sqlalchemy import create_engine
+
+        if not self.DB_TYPE:
+            self.DB_TYPE = "postgres"
+        
+        if self.DB_TYPE == "postgres":
+            conn_str: str = f'postgresql+psycopg2://{self.DB_HOST}:{self.DB_PASSWORD}@{self.DB_HOST}/{self.DB_PORT}'
+            engine = create_engine(conn_str)
+            return engine
